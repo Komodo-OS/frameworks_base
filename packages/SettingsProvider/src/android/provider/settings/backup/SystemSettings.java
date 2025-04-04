FortuneOS-CAF/platform_frameworks_base

base: Add three-fingers-swipe to screenshot [1/2]
The feature is ported from Oppo ColorOS.
With the options on, users can swipe down
on the screen to have a screenshot.
Original smali port by: wuxianlin

Change-Id: I87ed333f81e1259758ea899bf019061d8ab85fd0

base: Improvements for swipe to screenshot

* Cancel touch events when tap with three fingers

* Dyrex2004: Fix typos (Screnshot -> Screenshot)

Change-Id: I6e1228e4101aa357d1e26ca6d0db1c44428ef367

base: SwipeToScreenshot: Import MIUI implementation

Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys

idoybh (YAAP):
Improved code all across, remove the usage of a system prop and more...
also properly add co-authors

Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>
Co-Authored-By: wuxianlin <wuxianlinwxl@gmail.com>

Change-Id: I1d5938457b7cc4c5ef1fec235d27d8a40526961a
Co-Authored-By: Tobias Merkel <stylogey@gmail.com>
Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

Former-commit-id: 98e9a89b84291500772951f4282cd3d9f2618e89
Change-Id: Ida9bccf101c3a07581e05a9fd9f93978ffb0fd4b

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -121,7 +121,8 @@ public class SystemSettings {
                 Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
                 Settings.System.NETWORK_TRAFFIC_ARROW,
                 Settings.System.NETWORK_TRAFFIC_FONT_SIZE,
-                Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION
+                Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION,
+                Settings.System.THREE_FINGER_GESTURE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);

Komodo-OS/frameworks_base

Signed-off-by: wulan17 <wulan17@nusantararom.org>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -116,7 +116,8 @@ public class SystemSettings {
                 Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
-                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED
+                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
+                Settings.System.THREE_FINGER_GESTURE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);